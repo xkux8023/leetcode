7486bd20470f6b2ae121c757b0bf03014924f300
xkux8023 <326712928@qq.com>
2019-02-23 14:56:35 +0800

    解答领扣-557. 反转字符串中的单词 III

---
+++ code/string/lesson1.js
@@ -0,0 +1,29 @@
+// 557. 反转字符串中的单词 III
+
+// export default (str) => {
+//   let arr = str.split(' ')
+//   let result = arr.map(item => {
+//     return item.split('').reverse().join('')
+//   })
+//   return result.join(' ')
+// }
+
+// export default (str) => {
+//   return str.split(' ').map(item => {
+//     return item.split('').reverse().join('')
+//   }).join(' ')
+// }
+
+// export default (str) => {
+//   return str.split(/\s/g).map(item => {
+//     return item.split('').reverse().join('')
+//   }).join(' ')
+// }
+
+export default (str) => {
+  // 1. 字符串按空格进行分隔，保存数组，数组的元素的先后顺序就是单词的顺序
+  // 2. 对数组进行遍历，然后每个元素进行反转
+  return str.match(/[\w']+/g).map(item => {
+    return item.split('').reverse().join('')
+  }).join(' ')
+}
